18c93d2c50e837217aae54d3bfbf16e7c077791d
Benson Tran <benson.trn@gmail.com>
2018-04-19 15:03:42 -0400

    hopefully got random background

--- js/backgrounds.js
+++ js/backgrounds.js
@@ -1,14 +1,11 @@
-var imageURLS = [
-    "https://www.flickr.com/photos/bensonben/19322715828/in/album-72157650662329202/",
-    "https://www.flickr.com/photos/bensonben/15898159137/in/album-72157650662329202/",
-    "https://www.flickr.com/photos/bensonben/27956404733/in/album-72157650662329202/",
-    "https://www.flickr.com/photos/bensonben/16141850160/in/dateposted-public/"
-];
 
 function getURL() {
-    var img = 'background src=\"';
+    var imageURLS = [
+        "https://www.flickr.com/photos/bensonben/19322715828/in/album-72157650662329202/",
+        "https://www.flickr.com/photos/bensonben/15898159137/in/album-72157650662329202/",
+        "https://www.flickr.com/photos/bensonben/27956404733/in/album-72157650662329202/",
+        "https://www.flickr.com/photos/bensonben/16141850160/in/dateposted-public/"
+    ];
     var randomIndex = Math.floor(Math.random() * imageURLs.length + 1);
-    img += imageURLs[randomIndex];
-    img += '/>';
-    document.getElementById('background').innerHTML = img;
+    document.getElementById("random").style.background=imageURLS[randomIndex];
 }
